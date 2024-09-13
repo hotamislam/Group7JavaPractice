1e9e3cc80823e40f6f9f2a8c9c29be8d5ae4e900
ismailemira <ismailemaira@yahoo.com>
2024-09-13 13:43:49 -0500

    add Even or odd

--- src/week1/Ismail.java
+++ src/week1/Ismail.java
@@ -2,5 +2,28 @@ package week1;
 
 public class Ismail {
 
+    public static void main(String[] args) {
+        oddOrEven(5);
+        oddOrEven(6);
+    }
+
+    public static void oddOrEven (int n) {
+
+        if (n % 2 == 0){
+            System.out.println(n + " is Even");
+        }else{
+            System.out.println(n + " is Odd");
+        }
+
+    }
+
+    /*
+    numbers odd & even
+    write a method that can identify a given number is even or odd
+    EX:
+    5 -> odd    6 -> even
+
+     */
+
 
 }
