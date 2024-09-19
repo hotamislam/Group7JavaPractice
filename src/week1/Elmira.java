24d974daad86ceedff60ab345677b75d3ea213e6
elmatavi <bigdaelmira@gmail.com>
2024-09-19 21:18:14 +0200

    Added new Elmira class that has OddEven method

--- src/week1/Elmira.java
+++ src/week1/Elmira.java
@@ -1,4 +1,37 @@
 package week1;
 
 public class Elmira {
-}
+    public static void main(String[] args) {
+
+        /*
+        Numbers -- odd & even
+Write a method that can identify  a given number is even or odd.
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+         */
+
+
+        int n1 = 5;
+        System.out.println(n1+" is "+identifyOddEven(n1));
+
+
+        int n2 = 6;
+        System.out.println(n2+" is "+identifyOddEven(n2));
+
+
+
+
+    }
+
+
+        public static String identifyOddEven(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+
+
+
+}}
