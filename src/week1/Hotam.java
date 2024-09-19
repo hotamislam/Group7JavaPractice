3c6b6b6a33181aafb2f1df82dc1ae017b8117eaa
hotamislam <hotamislam@gmail.com>
2024-09-19 14:46:20 -0700

    added new method

--- src/week1/Hotam.java
+++ src/week1/Hotam.java
@@ -1,6 +1,18 @@
 package week1;
 
 public class Hotam {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println("The number " + number + " is " + identifyOddEven(number));
+    }
+
+  public static String identifyOddEven(int number) {
+      if (number % 2 == 0) {
+          return "Even";
+      } else {
+          return "Odd";
+      }
+  }
 
 
 
