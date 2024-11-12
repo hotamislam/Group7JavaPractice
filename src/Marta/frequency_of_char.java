6fd5b59483c8525de336b59e7fb098508b2f6e19
Marta Taran <marta.taran11@gmail.com>
2024-11-12 17:40:29 -0600

    Added FOC

---
+++ src/Marta/frequency_of_char.java
@@ -0,0 +1,57 @@
+package Marta;
+
+public class frequency_of_char {
+
+//Write a return method that can find the frequency of characters
+//Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+
+
+    public static void main(String[] args) {
+
+        String str = "AAABBCDD";
+        String result = "";
+
+
+
+
+        for (int i = 0; i < str.length(); i++) {
+
+            int count = 0;
+
+            String ch = str.charAt(i)+"";
+
+
+
+            //"AAABBCDD"
+            for(int j = 0; j < str.length(); j++) {
+
+                if (ch.equals(str.charAt(j) + "")) {
+                    count++;
+
+                }
+            }
+
+
+                if(result.contains(ch)){
+                    continue;
+                }
+
+                result += ch + count;
+
+
+
+
+
+        }
+        System.out.println(result);
+
+
+        }
+
+
+
+    }
+
+
+
+
